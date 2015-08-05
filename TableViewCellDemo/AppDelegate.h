32594070ddc4081aaee5f933b5083ae2439ad9b4
renxuan <renxuan@aikins-mbp.local>
2015-08-05 11:40:32 +0800

    Initial Commit

---
+++ TableViewCellDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewCellDemo
+//
+//  Created by renxuan on 15/8/5.
+//  Copyright (c) 2015年 renxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewCellDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TableViewCellDemo
+//
+//  Created by renxuan on 15/8/5.
+//  Copyright (c) 2015年 renxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
